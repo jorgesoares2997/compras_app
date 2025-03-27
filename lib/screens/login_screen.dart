4f79c91010f08ff1e18d47038eb4ff0e8c6ac890
jorge soares <jorgesoares@jorges-MacBook-Air.local>
2025-03-27 08:57:37 -0300

    github

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,9 +6,9 @@ import 'package:compras_app/services/report_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -22,13 +22,6 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _isLoading = false;
-
-  // Configuração do Google Sign-In
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-    clientId:
-        '911266742263-jm27q7p4v862mdic2p7ntacmpocutat8.apps.googleusercontent.com',
-    scopes: ['email', 'profile'],
-  );
 
   Future<void> testarRelatorios(BuildContext context) async {
     final reportService = ReportService();
@@ -72,61 +65,6 @@ class _LoginScreenState extends State<LoginScreen> {
           SnackBar(content: Text('${localizations.loginFailed}: $e')),
         );
       }
-    }
-  }
-
-  // Função para login com Google
-  Future<void> _loginWithGoogle(BuildContext context) async {
-    setState(() => _isLoading = true);
-    try {
-      print('Iniciando login com Google...');
-
-      // Verifica se já existe uma sessão ativa
-      final currentUser = await _googleSignIn.signInSilently();
-      if (currentUser != null) {
-        print('Usuário já está logado: ${currentUser.email}');
-        await _googleSignIn
-            .signOut(); // Força logout para garantir uma nova sessão
-      }
-
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      if (googleUser == null) {
-        print('Usuário cancelou o login com Google');
-        setState(() => _isLoading = false);
-        return;
-      }
-
-      print('Conta Google obtida: ${googleUser.email}');
-
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-      print('Token de acesso obtido: ${googleAuth.accessToken}');
-
-      if (googleAuth.accessToken == null) {
-        throw Exception('Token de acesso não obtido do Google');
-      }
-
-      final authService = Provider.of<AuthService>(context, listen: false);
-      final token = await authService.loginWithGoogle(googleAuth.accessToken!);
-
-      setState(() => _isLoading = false);
-      if (token != null) {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setString('token', token);
-        await prefs.setString('email', googleUser.email);
-        Navigator.pushReplacementNamed(context, '/main');
-      } else {
-        print('Falha no login com Google: token nulo');
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(const SnackBar(content: Text('Google login failed')));
-      }
-    } catch (e) {
-      setState(() => _isLoading = false);
-      print('Erro detalhado no login com Google: $e');
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Google login error: $e')));
     }
   }
 
@@ -279,57 +217,47 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                   ),
                   const SizedBox(height: 16),
-                  // Botão de Login com Google
-                  ElevatedButton.icon(
-                    onPressed:
-                        _isLoading ? null : () => _loginWithGoogle(context),
-                    icon: const Icon(Icons.g_mobiledata, color: Colors.black),
-                    label: const Text(
-                      'Login com Google',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      minimumSize: const Size(double.infinity, 50),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  // Botão de Login com GitHub
-                  ElevatedButton.icon(
-                    onPressed:
-                        _isLoading ? null : () => _loginWithGithub(context),
-                    icon: const Icon(Icons.code, color: Colors.black),
-                    label: const Text(
-                      'Login com GitHub',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      minimumSize: const Size(double.infinity, 50),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterScreen(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed:
+                            _isLoading ? null : () => _loginWithGithub(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(12),
                         ),
-                      );
-                    },
-                    child: Text(
-                      localizations.noAccountRegister,
-                      style: const TextStyle(color: Colors.black),
-                    ),
+                        child: const FaIcon(
+                          FontAwesomeIcons.github,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          child: const Text(
+                            'Registrar com Email',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
